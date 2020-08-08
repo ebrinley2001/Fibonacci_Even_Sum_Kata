6bd5a526251df660d61872126864e065cd2875d8
ebrinley2001 <brinleyethan@gmail.com>
2020-08-08 17:29:16 -0400

    1st Try

---
+++ FiboEven/FibonacciFunc.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace FiboEven
+{
+    public static class FibonacciFunc
+    {
+        public static int Fibo_Func(int num)
+        {
+            int sum = 0;
+            List<int> seq = new List<int>
+            {1,1};
+
+            for (int x = 0; seq[seq.Count - 1] < num; x++)
+            {
+                seq.Add(seq[x] + seq[x + 1]);
+            }
+
+            seq.RemoveAt(seq.Count - 1);
+
+            foreach (int fibNum in seq)
+            {
+                if (fibNum % 2 == 0)
+                {
+                    sum += fibNum;
+                }
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ FiboEvenTests/FibonacciFuncTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FiboEven;
+
+namespace FiboEven.Tests
+{
+    [TestClass()]
+    public class FibonacciFuncTests
+    {
+        [DataTestMethod]
+
+        [DataRow(20, 10)]
+        [DataRow(40, 44)]
+        [DataRow(4000000, 4613732)]
+
+
+        public void Fibo_Test(int a, int b)
+        {
+            Assert.AreEqual(b, FibonacciFunc.Fibo_Func(a));
+        }
+
+        
+    }
+}
